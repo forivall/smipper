f9aa7518b9cbf5e33a219b27af4af24e7da86abf
Anders Bakken <agbakken@gmail.com>
2023-07-11 11:04:26 -0700

    More rewriting of urls.

--- src/load.ts
+++ src/load.ts
@@ -2,23 +2,35 @@ import { Smipper } from "./Smipper";
 import { rewriteLocalControl } from "./rewriteLocalControl";
 import fs from "fs";
 import got from "got";
+import path from "path";
+import process from "process";
 
-export async function load(smipper: Smipper, path: string): Promise<string> {
-    smipper.verbose("load", path);
-    if (path.startsWith("http://localcontrol.netflix.com/")) {
-        path = rewriteLocalControl(smipper, path);
+export async function load(smipper: Smipper, filePath: string): Promise<string> {
+    smipper.verbose("load", filePath);
+    if (filePath.startsWith("http://localcontrol.netflix.com/")) {
+        filePath = rewriteLocalControl(smipper, filePath);
     }
 
-    if (path.startsWith("file:///")) {
-        return fs.readFileSync(path.substring(7), "utf8");
+    if (!filePath.includes("://")) {
+        if (filePath[0] === "/" && fs.existsSync(filePath)) {
+            filePath = `file://${filePath}`;
+        } else if (fs.existsSync(filePath)) {
+            filePath = `file://${path.join(process.cwd(), filePath)}`;
+        } else {
+            filePath = rewriteLocalControl(smipper, filePath);
+        }
     }
 
-    if (!path.startsWith("http://") && !path.startsWith("https://")) {
+    if (filePath.startsWith("file:///")) {
+        return fs.readFileSync(filePath.substring(7), "utf8");
+    }
+
+    if (!filePath.startsWith("http://") && !filePath.startsWith("https://")) {
         throw new Error("Not a url");
     }
 
     if (smipper.cache && smipper.cacheKey) {
-        const response = smipper.cache.get(smipper.cacheKey, path);
+        const response = smipper.cache.get(smipper.cacheKey, filePath);
         if (response) {
             return response;
         }
@@ -29,14 +41,14 @@ export async function load(smipper: Smipper, path: string): Promise<string> {
 
     // let retryIndex = 0;
     async function get(): Promise<string> {
-        const response = await got.get(path, { timeout: 10000 });
+        const response = await got.get(filePath, { timeout: 10000 });
         smipper.verbose(response.headers);
         return response.body || "";
     }
 
     const response = await get();
     if (smipper.cacheEntry) {
-        smipper.cacheEntry.add(path, response);
+        smipper.cacheEntry.add(filePath, response);
     }
     return response;
 }
